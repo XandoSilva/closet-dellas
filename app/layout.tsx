4fa7f67f5ac04cb227e130ee2e9071cc05c66e9a
Alessandro Souza <adasilvasouza@gmail.com>
2026-03-23 11:46:23 -0300

    23-03 / 3.2 link whats

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,24 @@
+// Arquivo: app/layout.tsx
+
 import './globals.css';
 
 export const metadata = {
   title: 'Closet Dellas | Moda Feminina Premium',
-  description: 'Curadoria exclusiva das melhores tendências do Brás, unindo sofisticação e preço justo para mulheres reais.',
+  description: 'Curadoria exclusiva das melhores tendências, unindo sofisticação e preço justo para mulheres reais.',
   openGraph: {
     title: 'Closet Dellas | Moda Feminina Premium',
-    description: 'A elegância que você merece. Direto do Brás para o seu guarda-roupa.',
+    description: 'A elegância que você merece. Peças exclusivas selecionadas para o seu guarda-roupa.',
+    url: 'https://closet-dellas.vercel.app/',
+    siteName: 'Closet Dellas',
     images: [
       {
-        url: 'https://res.cloudinary.com/dwnaedy9f/image/upload/v1774216251/Logo_ClosetDellas_sem_fundo_h4fvys.png', // <--- COLE O LINK DA SUA LOGO AQUI!
+        url: 'https://res.cloudinary.com/dwnaedy9f/image/upload/v1774216251/Logo_ClosetDellas_sem_fundo_h4fvys.png',
         width: 1200,
         height: 630,
         alt: 'Logo Closet Dellas',
       },
     ],
+    locale: 'pt_BR',
     type: 'website',
   },
 };
